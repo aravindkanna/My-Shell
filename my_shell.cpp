254d11b50d4e123969c00cfd661534929e92cddb
Aravind Reddy <aravindreddy255@gmail.com>
2016-05-06 16:56:25 +0530

    complete code

--- my_shell.cpp
+++ my_shell.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <sstream>
 #include <sys/wait.h>
+#include <fcntl.h>
 
 using namespace std;
 
@@ -13,66 +14,98 @@ int main(int argc, char* argv[]){
 	string command;
 	pid_t pid;
 	int status;
+	string input_file = "";
+	string output_file = "";
 	while(1){
 		cout << "my_shell $: ";
 		getline(cin, command);
 		vector<string> splt_cmnd;
-		int TYPE = 0;
+		vector<bool> TYPE(3);
 		pid = fork();//this creates a new child
 		int combd;
 		if(pid){
 			waitpid(pid, &status, WUNTRACED);
-			getchar();
 		}
 		else{
 			stringstream ss(command);
 			string token;
-			//combd = 0;
 			while(getline(ss, token, ' ')){
 				combd=0;
 				if(token.length()){
+					if(splt_cmnd.size()){
+						string bck = splt_cmnd.back();
+						if(!strcmp(bck.c_str(), "<")){
+							input_file = token;
+							splt_cmnd.pop_back();
+						}
+						if(!strcmp(bck.c_str(), ">") || !strcmp(bck.c_str(), ">>")){
+							output_file = token;
+							splt_cmnd.pop_back();
+						}
+					}
 					splt_cmnd.push_back(token);
 					if(token[0]=='<'){
-						TYPE = 1;
+						TYPE[0] = true;
 					}
 					else if(token[0] == '>'){
-						TYPE = 2;
+						TYPE[1] = true;
 						if(token[1]){
 							if(token[1] == '>'){
-								TYPE = 3;
+								TYPE[1] = false;
+								TYPE[2] = true;
 							}
 						}
 					}
 					if(token[0]=='<' && token[1] && token[1]!=' '){
 						int len = splt_cmnd.size();
 						string t = splt_cmnd[len-1];
-						splt_cmnd[len-1] = t.substr(0, 1);
-						splt_cmnd.push_back(t.substr(1));
+						splt_cmnd[len-1] = t.substr(1);
+						input_file = splt_cmnd.back();
 					}
-					else if(token[0]=='>' && TYPE==2 && token[1] && token[1]!=' '){
+					else if(token[0]=='>' && TYPE[1] && token[1] && token[1]!=' '){
 						int len = splt_cmnd.size();
 						string t = splt_cmnd[len-1];
 						splt_cmnd[len-1] = t.substr(0, 1);
 						splt_cmnd.push_back(t.substr(1));
+						output_file = splt_cmnd.back();
 					}
 					if(token[0]=='>' && token[1]=='>' && token[2] && token[2]!=' '){
 						int len = splt_cmnd.size();
 						string t = splt_cmnd[len-1];
-						splt_cmnd[len-1] = t.substr(0, 2);
-						splt_cmnd.push_back(t.substr(2));
+						splt_cmnd[len-1] = t.substr(2);
+						output_file = splt_cmnd.back();
 					}	
+
 				}
 			}
-		}
-		/*for(int i=0;i<splt_cmnd.size();i++){
-			cout << splt_cmnd[i] << endl;
-		}*/
 
-		/***
-			TYPE-1 : taking a file as input
-			TYPE-2 : writing the contents to the file by overriding
-			TYPE-3 : writing the contents by appending
-		***/
+			int len=splt_cmnd.size();
+			if(TYPE[0]){
+				int ip = open(input_file.c_str(), O_RDONLY);
+				dup2(ip, 0);
+				close(ip);
+
+
+			}
+			if(TYPE[1]){
+				remove(output_file.c_str());
+				int op = open(output_file.c_str(), O_WRONLY | O_CREAT, S_IRWXU);
+				dup2(op, 1);
+				close(op);
+			}
+			if(TYPE[2]){
+				int op = open(output_file.c_str(), O_APPEND | O_WRONLY | O_CREAT, S_IRWXU);
+				dup2(op, 1);
+				close(op);
+			}
+
+			char *a[len];
+			for(int i=0;i<len;i++){
+				a[i] = strdup(splt_cmnd[i].c_str());
+			}
+			a[len] = NULL;
+			execvp(a[0], a);
+		}
 	}
 	return 0;
 }
